fff0dd4cd52f134d902e070bb7008cde0d6e5ccd
Andrew Snead <andrewcsnead@gmail.com>
2014-12-17 16:10:32 +1100

    credit card example

---
+++ src/credit_card.py
@@ -0,0 +1,33 @@
+class ClassCard:
+
+  def __init__(self, customer, bank, acnt, limit):
+    self._customer = customer
+    self._bank = bank
+    self._account = acnt
+    self._limit = limit
+    self._balance = 0
+
+  def get_customer(self):
+    return self._customer
+
+  def get_bank(self):
+    return self._bank
+
+  def get_account(self):
+    return self._account
+
+  def get_limit(self):
+    return self._limit
+
+  def get_balance(self):
+    return self._balance
+
+  def charge(self, price):
+    if price + self._balance > self._limit:
+      return False
+    else: 
+      self._balance += price
+      return True
+
+  def make_payment(self, amount):
+    self._balance -= amount
