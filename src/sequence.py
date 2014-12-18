d8d9a44c96c67a84a2210ad827f01a28eed63655
Andrew Snead <andrewcsnead@gmail.com>
2014-12-19 10:00:46 +1100

    simple range clasS

--- src/sequence.py
+++ src/sequence.py
@@ -1,15 +1,24 @@
-class SequenceIterator:
+class Range:
 
-  def __init__(self, sequence):
-    self._seq = sequence
-    self._k = -1
+    def __init__(self, start, stop=None, step=1):
+        if step == 0:
+            raise ValueError('step cannot be 0')
+        if stop is None:
+            start, stop = 0, start
 
-  def __next__(self):
-    self._k += 1
-    if self._k < len(self._seq):
-      return(self._seq[self._k])
-    else:
-      raise StopIteration()
+        self._length = max(0, (stop - start + step -1) // step)
 
-  def __iter__(self):
-    return self
+        self._start = start
+        self._step = step
+
+    def __len__(self):
+        return self._length
+
+    def __getitem__(self, k):
+        if k < 0:
+            k += len(self)
+
+        if not 0 <= k < self._length:
+            raise IndexError('index out of range')
+
+        return self._start + k * self._step
