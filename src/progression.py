25796c25c08ae4cb3a6a1cd253090aedf750c7f0
Andrew Snead <andrewcsnead@gmail.com>
2014-12-20 10:08:11 +1100

    added extensions to the progression class

--- src/progression.py
+++ src/progression.py
@@ -29,3 +29,13 @@ class ArithmeticProgression(Progression):
     def _advance(self):
         self._current += self._increment
 
+class GeometricProgression(Progression):
+
+    def __init__(self, base=2, start=1):
+
+        super().__init__(start)
+        self._base = base
+
+    def _advance(self):
+        self._current *= self._base
+
