e2717b1bfc93521a0ad38c9745913b1d5fd384dd
Andrew Snead <andrewcsnead@gmail.com>
2014-12-20 10:13:23 +1100

    simple fibonacci progression

--- src/progression.py
+++ src/progression.py
@@ -32,10 +32,18 @@ class ArithmeticProgression(Progression):
 class GeometricProgression(Progression):
 
     def __init__(self, base=2, start=1):
-
         super().__init__(start)
         self._base = base
 
     def _advance(self):
         self._current *= self._base
+
+class FibonacciProgression(Progression):
+
+    def __init__(self, first=0, second=1):
+        super().__init__(first)
+        self._prev = second - first
+
+    def _advance(self):
+        self._prev, self._current = self._prev + self._current
 
