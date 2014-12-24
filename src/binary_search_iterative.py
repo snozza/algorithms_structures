fc018ee13705bd4f64cb7adc0dceb0b94b92eaec
Andrew Snead <andrewcsnead@gmail.com>
2014-12-24 18:51:19 +1100

    iterative version of binary search

---
+++ src/binary_search_iterative.py
@@ -0,0 +1,13 @@
+def binary_search_iterative(data, target):
+    """Return True if target is found in list"""
+    low = 0
+    high = len(data)-1
+    while low <= high:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
