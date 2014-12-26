3d6519c5fd9ec8d90d38f2d49bd268073ae173ce
Andrew Snead <andrewcsnead@gmail.com>
2014-12-26 23:42:23 +1100

    add linked deque based on doubly linked list

--- src/doubly_linked_base.py
+++ src/doubly_linked_base.py
@@ -40,3 +40,33 @@ class _DoublyLinkedBase:
         node._prev = node._next = node._element = None
         return element
 
+class LinkedDeque(_DoublyLinkedBase):
+    """Double-ended queue implementation based on doubly linked list"""
+
+    def first(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._header._next._element
+
+    def last(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._trailer._prev._element
+
+    def insert_first(self, e):
+        """Add an element to the front of the deque"""
+        self._insert_between(e, self._header, self._header._next)
+
+    def insert_last(self, e):
+        self._insert_between(e, self._trailer._prev, self._trailer)
+
+    def delete_first(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._delete_node(self._header._next)
+
+    def delete_last(self):
+        if self.is_empty():
+            raise Empty('deque is empty')
+        return self._delete_node(self._trailer._prev)
+
