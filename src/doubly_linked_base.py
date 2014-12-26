456c82e1dedae03f6100ed815f203e54823b5402
Andrew Snead <andrewcsnead@gmail.com>
2014-12-27 10:21:30 +1100

    working on Positional List ADT

--- src/doubly_linked_base.py
+++ src/doubly_linked_base.py
@@ -70,3 +70,56 @@ class LinkedDeque(_DoublyLinkedBase):
             raise Empty('deque is empty')
         return self._delete_node(self._trailer._prev)
 
+class PositionalList(_DoublyLinkedBase):
+    """sequential container of elements allowing positional access"""
+    class Position:
+        """An abstraction representing location of single element"""
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user"""
+            self._container = container
+            self._node = node
+
+        def element(self):
+            """Return the element stored at this Position"""
+            return self._node._element
+
+        def __eq__(self, other):
+            """Return True if other does not represent the same location"""
+            return type(other) is type(self) and other._node is self._node
+
+        def __ne__(self, other):
+            """Return True is other does not represent the same location"""
+            return not(self == other)
+
+    #-------------utility methods---------------
+    def _validate(self, p):
+        """Return position's node, or raise appropriate error if invalid"""
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+        if p._node._next is None:
+            raise ValueError('p is no longer valid')
+        return p._node
+
+    def _make_position(self, node):
+        """Return Position instance for given node (or None if sentinel"""
+        if node is self._header or node is self._trailer:
+            return None
+        else:
+            return self.Position(self, node)
+
+    #--------------accessors----------------
+    def first(self):
+        return self._make_position(self._header._next)
+
+    def last(self):
+        return self._make_position(self._trailer._prev)
+
+    def before(self, p):
+        """Return Position just before Position p"""
+        node = self._validate(p)
+        return self._make_position(node._prev)
+
+
+
