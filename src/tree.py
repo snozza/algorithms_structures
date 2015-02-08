307bae717f9f428f7cb1d40de48a6a8a5e5a0d6e
Andrew Snead <andrewcsnead@gmail.com>
2015-02-08 13:30:25 +0000

    added traversal methods to tree clasS

--- src/tree.py
+++ src/tree.py
@@ -50,5 +50,17 @@ class Tree:
     def is_empty(self):
         """Return True is the tree is empty"""
         return len(self) == 0
+
+    def preorer(self):
+      if not self.is_empty():
+        for p in self._subtree_preorder(self.root()):
+          yield p
+
+    def _subtree_preorder():
+      yield p
+      for c in self.children(p):
+        for other in self._subtree_preorder(c):
+          yield other
+
         
 
